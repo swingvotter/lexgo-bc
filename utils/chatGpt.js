e2546f23bcc943f15c94f0e98e9b2cdc44943305
swingvotter <swingvotter@gmail.com>
2025-12-23 17:39:40 +0000

    intergrated openAi and created chatGpt in utils

---
+++ utils/chatGpt.js
@@ -0,0 +1,19 @@
+const OpenAI = require("openai")
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function chatGpt(question) {
+  const response = await client.chat.completions.create({
+    model: "gpt-5-mini",
+    messages: [
+      { role: "system", content: "You answer questions clearly." },
+      { role: "user", content: question }
+    ],
+  });
+
+  console.log(response.choices[0].message.content);
+}
+
+module.exports = chatGpt
